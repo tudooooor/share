3e111d385237c90b8829c41638a39a29d287f3c2
tudooooor <share_us@outlook.com>
2017-11-23 19:25:47 +0800

    update xman for client. xman-admin for server. doc for dairy MOM. tp5 for SW framework.

---
+++ tp5/index.php
@@ -0,0 +1,8 @@
+<?php
+define('APP_NAME','Apps');
+define('APP_PATH','./Apps/');
+define('PAY_OUT_TIME',1800);
+define('APP_DEBUG',true);
+define('RUNTIME_PATH',APP_PATH.'temp/');
+require './ThinkPHP/start.php';
+?>
