1c4223e2bc68868a775e9d8ff710b440fbc5c96d
taolin <32774031+tudooooor@users.noreply.github.com>
2018-05-15 00:15:13 +0800

    修茬

--- xman/source/pages/homePage/homePage.js
+++ xman/source/pages/homePage/homePage.js
@@ -8,9 +8,9 @@ Page({
    */
   data: {
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      '../../utils/images/1.jpg',
+      '../../utils/images/2.jpg',
+      '../../utils/images/3.jpg'
     ],
     indicatorDots: true,
     autoplay: true,
