8b90083fc36d31266bff2e20a223b1b3caf3f6d1
taolin <32774031+tudooooor@users.noreply.github.com>
2018-09-11 09:19:15 +0800

    增加三级菜单显示

--- xman/source/pages/saleService/saleService.js
+++ xman/source/pages/saleService/saleService.js
@@ -1,6 +1,8 @@
 Page({
   data: {
     maskShow:'none',
+    flag: true,
+    showShare:'none',
 
     list01: [
       { item_id: 1 }, { item_id: 11 }, { item_id: 11 },
@@ -16,7 +18,8 @@ Page({
     ],
 
   // 展开折叠
-    selectedFlag: [false, false, false, false],
+    selectedFlag: [false, false, false, false,false, false, false, false],
+    selectedF:[false, false, false, false,false, false, false, false],
 
   },
     // 展开折叠选择  
@@ -32,4 +35,45 @@ Page({
       selectedFlag: this.data.selectedFlag
     })
   },
+  change2Toggle:function(e){
+    var index = e.currentTarget.dataset.index;
+    if (this.data.selectedF[index]){
+      this.data.selectedF[index] = false;
+    }else{
+      this.data.selectedF[index] = true;
+    }
+
+    this.setData({
+      selectedF: this.data.selectedF
+    })
+  },
+
+ show: function () {
+    this.setData({ flag: false })
+  },
+  hide: function () {
+    this.setData({ flag: true })
+  },
+
+  maskHide:function(){
+    this.data.goodSpecifications = '';
+    for (var index = 0; index < 10; index++)
+    // {
+    //   if (this.data.count[index] != 0)
+    //   {
+    //     this.data.goodSpecifications += ('规格:' + this.data.goodCategorys[index].specifications + ' ' + this.data.count[index] + '件') + ' ';
+    //   }
+    // }
+    this.setData({
+      maskShow:'none',
+      goodSpecifications: this.data
+    });
+
+  },
+  maskDisplay:function() {
+    this.setData({
+      maskShow: 'flex'
+    });
+  },
+
 })
